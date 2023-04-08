54cbf0a95c89775308a8e9962b070d1a71473f6f
Bahhe <marchelldteach@gmail.com>
2023-04-08 21:14:12 +0100

    fixed bugs

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,19 +1,44 @@
 import Image from "next/image";
 
-const url = ["1", "2", "3", "4", "5", "6"];
-
 const Images = () => {
   return (
     <section className="w-full pt-28">
-      {url.map((u, i) => (
-        <Image
-          src={`/${u}.webp`}
-          width={1920}
-          height={1280}
-          alt="product image"
-          key={i}
-        />
-      ))}
+      <Image
+        src={"/product/one.webp"}
+        width={1920}
+        height={1280}
+        alt="product image"
+      />
+      <Image
+        src={"/product/two.webp"}
+        width={1920}
+        height={1280}
+        alt="product image"
+      />
+      <Image
+        src={"/product/three.webp"}
+        width={1920}
+        height={1280}
+        alt="product image"
+      />
+      <Image
+        src={"/product/four.webp"}
+        width={1920}
+        height={1280}
+        alt="product image"
+      />
+      <Image
+        src={"/product/five.webp"}
+        width={1920}
+        height={1280}
+        alt="product image"
+      />
+      <Image
+        src={"/product/six.webp"}
+        width={1920}
+        height={1280}
+        alt="product image"
+      />
     </section>
   );
 };
