947655f8439d176171a4eed9f9577d74638e131b
Bahhe <marchelldteach@gmail.com>
2023-04-16 03:16:01 +0100

    added facebook pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+import Script from "next/script";
+// import { fbq } from "react-facebook-pixel";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -15,25 +17,43 @@ const poppins = Poppins({
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    // eslint-disable-next-line
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("771903037595646"); // facebookPixelId
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
-  }, [router.events]);
+  // useEffect(() => {
+  //   // eslint-disable-next-line
+  //   import("react-facebook-pixel")
+  //     .then((x) => x.default)
+  //     .then((ReactPixel) => {
+  //       ReactPixel.init("771903037595646"); // facebookPixelId
+  //       ReactPixel.pageView();
+  //
+  //       router.events.on("routeChangeComplete", () => {
+  //         ReactPixel.pageView();
+  //       });
+  //     });
+  // }, [router.events]);
 
   return (
     <main className={`${poppins.variable} overflow-hidden font-sans`}>
       <ClerkProvider {...pageProps}>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '938487093952661');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </ClerkProvider>
     </main>
